50e26a4aa334a4cfa796973a5a75357fde2578c3
Irfan Nadhir <irfannadhir1@gmail.com>
2022-12-12 14:36:42 +0700

    penggajian done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -161,10 +161,6 @@
                                         Settings</a></li>
                                 <li><a href="#!" class="dropdown-item"><i class="feather icon-user"></i>
                                         Profile</a></li>
-                                <li><a href="message.html" class="dropdown-item"><i class="feather icon-mail"></i> My
-                                        Messages</a></li>
-                                <li><a href="auth-signin.html" class="dropdown-item"><i
-                                            class="feather icon-lock"></i> Lock Screen</a></li>
                             </ul>
                         </div>
                     </div>
